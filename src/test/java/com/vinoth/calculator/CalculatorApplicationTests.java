bf341f00c2f288b0fd05be7472980ed5b0b14f56
Vinoth Kumar Chinniah Mani (Digital) <vi390699@Vinoths-MacBook-Pro-2.local>
2017-09-25 15:33:50 +0530

    Add sum logic, controller and unit test

--- src/test/java/com/vinoth/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/vinoth/calculator/CalculatorApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.Assert.assertEquals;
+
+
 public class CalculatorApplicationTests {
 
+	private Calculator calculator = new Calculator();
+
 	@Test
-	public void contextLoads() {
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
 	}
 
 }
